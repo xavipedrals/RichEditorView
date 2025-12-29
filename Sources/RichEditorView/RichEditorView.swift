d7dada9c057fc88ef8127331e608285533702067
Xavi Pedrals <xavi.pedrals@gmail.com>
2025-12-29 08:49:12 +0100

    Added insert HTML func

--- Sources/RichEditorView/RichEditorView.swift
+++ Sources/RichEditorView/RichEditorView.swift
@@ -426,6 +426,11 @@ public class RichEditorWebView: WKWebView {
         runJS("RE.setCheckbox('\(UUID().uuidString.prefix(8))')")
     }
     
+    public func insertHtml(_ html: String) {
+        runJS("RE.prepareInsert()")
+        runJS("RE.insertHTML('\(html.escaped)')")
+    }
+
     public func insertText(_ text: String) {
         runJS("RE.prepareInsert()")
         runJS("RE.insertText('\(text.escaped)')")
